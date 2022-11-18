8083429775821de4ed09f606e6e22a6d73656c09
Lucca Rauédys <luccarauedys@outlook.com>
2022-11-18 19:53:35 -0300

    feat: query clients by name (optional param)

--- src/graphql/resolvers/client.ts
+++ src/graphql/resolvers/client.ts
@@ -1,9 +1,14 @@
 import { Client } from "../../models/client";
 
-const client = async (_: any, { id }: { id: String }) => {
+const client = async (_: any, { id }: { id: string }) => {
   return await Client.findById(id);
 };
 
+const clients = async (_: any, { name = "" }: { name: string }) => {
+  const regex = new RegExp(name);
+  return await Client.find({ name: regex });
+};
+
 export const clientResolvers = {
-  Query: { client },
+  Query: { client, clients },
 };
